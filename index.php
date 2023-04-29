961d4da0fe91da755903bf4e49f75a4d4a8a63eb
Ravi Rajvanshi <rajvanshiravi@gmail.com>
2023-04-29 22:07:04 +0530

    Author Updation

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
- *
  * @package WordPress
+ * By Ravi Rajvanshi
  */
 
 /**
